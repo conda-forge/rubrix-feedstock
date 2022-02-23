b274feeeee45127804c7b0b76f0102d8cef60cfa
Sugato Ray <sugatoray@users.noreply.github.com>
2022-02-22 18:16:02 -0600

    update recipe

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# Passing no-deps is necessary to avoid the following type of build error
+# "ClobberWarning: This transaction has incompatible packages due to a shared path."
 ${PYTHON} -m pip install . -vv \
-    # passing no-deps is necessary to avoid the following type of build error
-    # "ClobberWarning: This transaction has incompatible packages due to a shared path."
     --no-deps \
     # --no-index \
     # --ignore-installed \
