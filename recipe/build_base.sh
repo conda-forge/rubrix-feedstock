cacb29d026f95605370e5544e413bbaaea5e8693
Sugato Ray <sugatoray@users.noreply.github.com>
2022-02-22 18:25:46 -0600

    update recipe: cleaup 01

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -2,7 +2,4 @@
 
 # Passing no-deps is necessary to avoid the following type of build error
 # "ClobberWarning: This transaction has incompatible packages due to a shared path."
-${PYTHON} -m pip install . -vv \
-    --no-deps \
-    # --no-index \
-    # --ignore-installed \
+${PYTHON} -m pip install . -vv --no-deps
